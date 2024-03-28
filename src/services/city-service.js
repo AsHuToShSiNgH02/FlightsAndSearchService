f6feb63ca01407e868642a5834dc12bbf8ca301e
AsHuToShSiNgH02 <ashutoshin2002@gmail.com>
2024-03-28 11:46:05 +0530

    Added city service

---
+++ src/services/city-service.js
@@ -0,0 +1,50 @@
+const {CityRepository} = require('../respository/index');
+
+class CityService{
+
+    constructor(){
+        this.cityrepository = new CityRepository();
+    }
+
+    async createCity(data) {
+        try {
+            const city = await this.cityrepository.createCity(data);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw(error);
+        }
+    }
+
+    async deleteCity(cityId) {
+        try {
+            const response = await this.cityrepository.deleteCity(cityId);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw(error);
+        }
+    }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await this.cityrepository.updateCity(cityId, data);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw(error);
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await this.cityrepository.getCity(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw(error);
+        }
+    }
+}
+
+module.exports = CityService;
